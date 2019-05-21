9eb99dc69728681699b5242ca138e9dc97535d98
99691457 <50843121+99691457@users.noreply.github.com>
2019-05-21 11:36:48 -0700

    Update headTails.ino

--- headTails.ino
+++ headTails.ino
@@ -1,2 +1,3 @@
 long randNumber; String result; int headCount; int tailCount; void setup() {Serial.begin(9600); randomSeed(analogRead(0));} void loop() {randNumber = random(2); if (randNumber == 1){ result= "Heads"; headCount += 1;} else{ result = "Tails"; tailCount += 1;} Serial.println(result); if (headCount == 100){ Serial.println("Heads wins"); Serial.print("Heads won by "); Serial.println(headCount - tailCount); delay(10000); tailCount = 0; headCount = 0; } if (tailCount == 100){ Serial.println("Tails wins"); Serial.print("Tails won by "); Serial.println(tailCount - headCount); delay(5000); tailCount = 0; headCount = 0;} delay(15);}
 //to show my skill, i did everything on one line
+//this is a heads or tails game
